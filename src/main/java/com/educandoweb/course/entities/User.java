8f006b56b68c758ad970423e4f7dd5c4e0429c6e
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-03 20:58:03 -0300

    User update

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	private String name;
 	private String email;
 	private String phone;
 	private String password;
@@ -36,10 +36,10 @@ public class User implements Serializable{
 		
 	}
 
-	public User(Long id, String nome, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
@@ -53,12 +53,12 @@ public class User implements Serializable{
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
